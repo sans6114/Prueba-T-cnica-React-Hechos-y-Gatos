c3c8395328b8a93d1493302290ed8e725100bbaf
Santiago Sosa <santisosalp@gmail.com>
2024-09-18 20:38:19 -0300

    mejoramos el codigo y modificamos el hook

--- src/AppGatos.jsx
+++ src/AppGatos.jsx
@@ -4,18 +4,13 @@ import { useFetchHook } from './hooks/useFetchHook';
 
 export function AppGatos() {
 
-  const URL_FACTS = 'https://catfact.ninja/fact'
-  const { data, isLoading, urlImage } = useFetchHook(URL_FACTS)
-  const fact = data
-console.log(urlImage)
-
-
+  const { fact, image, isLoading } = useFetchHook()
 
   return (
     <main className='flex flex-col items-center justify-center'>
       <h1 className="mt-10 mb-[100px] text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">React Gatos</h1>
       {/* renderizado condicional del facto extraido de mi API PUBLICA */}
-      {isLoading ? (<Loading />) : (<Card img={urlImage} quote={fact}/>)}
+      {isLoading ? (<Loading />) : (<Card img={image} quote={fact} />)}
     </main>
   )
 }
--- src/hooks/useFetchHook.js
+++ src/hooks/useFetchHook.js
@@ -3,77 +3,43 @@ import {
   useState,
 } from 'react';
 
-export const useFetchHook = (url) => {
-    const [urlImage, setUrlImage] = useState('')
-    const [state, setState] = useState({
-        data: null,
-        isLoading: true,
-    })
-    const { data, isLoading } = state
+//URLS
+const URL_FACTS = 'https://catfact.ninja/fact'
+const URL_IMAGE = 'https://cataas.com/cat/says/'
+export const useFetchHook = () => {
+
+
+    const [fact, setFact] = useState(null)
+    const [image, setImage] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+
 
     useEffect(() => {
+
+        const fetchUrl = async () => {
+            try {
+                setIsLoading(true)
+                const res = await fetch(URL_FACTS)
+                const factData = await res.json()
+                setFact(factData.fact)
+
+                //my first tree words
+                const words = factData.fact.split(' ', 3).join(' ')
+                const imageUrl = `${URL_IMAGE}${encodeURIComponent(words)}`
+                setImage(imageUrl)
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
         fetchUrl()
     }, [])
 
-    useEffect(() => {
-        if (data) {
-            fetchImage();
-        }
-    }, [data]);
-
-
-    const fetchImage = async () => {
-
-        try {
-            const res = await fetch(`https://cataas.com/cat/says/${data}`)
-            if (!res.ok) {
-                console.error('Error fetching image:', res.statusText);
-            }
-            setUrlImage(res.url)
-        } catch (error) {
-            console.error('Fetch error:', error);
-        }
-
-    }
-    const setLoadingState = () => {
-        setState({
-            data: null,
-            isLoading: true,
-        })
-    }
-    const fetchUrl = async () => {
-        try {
-            setLoadingState()
-            const res = await fetch(url)
-            await new Promise(resolve => setTimeout(resolve, 1000))
-            if (!res.ok) {
-                setState({
-                    data: null,
-                    isLoading: false,
-                })
-            }
-            const data = await res.json()
-            console.log(data.fact)
-            // Divide el hecho en un array de palabras y toma las primeras 3
-            const words = data.fact.split(' ', 3).join(' ')
-            console.log(words)
-            setState({
-                data: words,
-                isLoading: false,
-            })
-        } catch (error) {
-            setState({
-                data: null,
-                isLoading: false,
-            });
-        }
-    }
-
-
 
     return {
-        data,
-        isLoading,
-        urlImage
+        fact,
+        image,
+        isLoading
     }
 }
